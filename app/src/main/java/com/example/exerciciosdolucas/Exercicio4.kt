363fe99853cf95f0f1e77d0a8d522123ec432242
V1n1c1us-KS-W4t4n4b3 <vinikswatanabe@outlook.com>
2023-09-16 21:50:38 -0300

    criação de todas as emptys q vao ser usadas

---
+++ app/src/main/java/com/example/exerciciosdolucas/Exercicio4.kt
@@ -0,0 +1,11 @@
+package com.example.exerciciosdolucas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Exercicio4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercicio4)
+    }
+}
---
+++ app/src/main/java/com/example/exerciciosdolucas/Exercicio7.kt
@@ -0,0 +1,11 @@
+package com.example.exerciciosdolucas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Exercico7 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercico7)
+    }
+}
