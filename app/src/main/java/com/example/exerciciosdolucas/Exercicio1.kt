a7e026003098e72ed13fe45801b548b2bc8f8a06
V1n1c1us-KS-W4t4n4b3 <vinikswatanabe@outlook.com>
2023-09-17 03:44:27 -0300

    exercicio 2

--- app/src/main/java/com/example/exerciciosdolucas/Exercicio1.kt
+++ app/src/main/java/com/example/exerciciosdolucas/Exercicio1.kt
@@ -2,10 +2,59 @@ package com.example.exerciciosdolucas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import com.example.exerciciosdolucas.databinding.ActivityExercicio1Binding
 
 class Exercicio1 : AppCompatActivity() {
+    private lateinit var binding: ActivityExercicio1Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_exercicio1)
+        binding = ActivityExercicio1Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonCalcular.setOnClickListener {
+            calcularMostrar()
+        }
+    }
+
+    fun somar(v1: Double, v2: Double): Double {
+        return v1 + v2
+    }
+
+    fun subtrair(v1: Double, v2: Double): Double {
+        return v1 - v2
+    }
+
+    fun multiplicar(v1: Double, v2: Double): Double {
+        return v1 * v2
+    }
+
+    fun dividir(v1: Double, v2: Double): Double {
+        return v1 / v2
+    }
+
+    fun calcularMostrar() {
+        val editPrimeiro = binding.editnumb1
+        val editSegundo = binding.editnumb2
+        val textSoma = binding.textSoma
+        val textSub = binding.textSub
+        val textMult = binding.textMult
+        val textDiv = binding.textDiv
+
+        val valor1 = editPrimeiro.text.toString().toDouble()
+        val valor2 = editSegundo.text.toString().toDouble()
+
+        val result1 = somar(valor1, valor2)
+        textSoma.text = result1.toString()
+
+        val result2 = subtrair(valor1, valor2)
+        textSub.text = result2.toString()
+
+        val result3 = multiplicar(valor1, valor2)
+        textMult.text = result3.toString()
+
+        val result4 = dividir(valor1, valor2)
+        textDiv.text = result4.toString()
     }
 }
--- app/src/main/java/com/example/exerciciosdolucas/Exercicio2.kt
+++ app/src/main/java/com/example/exerciciosdolucas/Exercicio2.kt
@@ -2,10 +2,62 @@ package com.example.exerciciosdolucas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.exerciciosdolucas.databinding.ActivityExercicio2Binding
 
 class Exercicio2 : AppCompatActivity() {
+    private lateinit var binding: ActivityExercicio2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_exercicio2)
+        binding = ActivityExercicio2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonCalcular.setOnClickListener {
+            tabuada()
+        }
+    }
+
+    fun tabuada() {
+
+        // é onde bou escrever o numero da tabuada
+        val textTabu = binding.editTabu
+
+        // na vddera pra sair o resultado aqui
+        val resultado = binding.resultTabu
+
+        // converte o string do numero da tabuada em double
+        val valor = textTabu.text.toString().toDouble()
+
+        // passa o valor convertido para a variavel v1
+        val v1 = valor
+
+        // esse é a variavel q vai guardar o resultado da formula do println
+        var resultadoFinal = ""
+
+        // determina a sequencia de um numero minino ate um numero maximo
+        for (i in 1..10) {
+
+            // ela esta recebendo o resultado do valor digitado sendo multiplicado pelo numero da sequencia
+            val product = v1 * i
+
+            // a formula do numero digitado vezes o numero minimo ate o numero maximo dentro de uma variavel
+            val formula = "$v1 * $i = $product"
+
+            // ele imprimi o calculo no logcat
+            println(formula)
+
+            //a formula multiplicando o numero da tabuada em ate o numero de vezes escolhido em looping e quebrnado a linha para ser armazenado no resultfinal
+            resultadoFinal += "$formula \n"
+
+
+        }
+        // aqui vai mostrar o que o resultado final guardou no text resultado
+        resultado.text = resultadoFinal
     }
 }
+
+
+// para ser armazenado
+// guardou no
+// dentro
+// que vai guardar
