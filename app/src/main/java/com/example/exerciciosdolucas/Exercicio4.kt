594fc1357d47444efda0a69f7e277bb7bde54283
V1n1c1us-KS-W4t4n4b3 <vinikswatanabe@outlook.com>
2023-09-17 21:21:46 -0300

    exercicio 4

--- app/src/main/java/com/example/exerciciosdolucas/Exercicio4.kt
+++ app/src/main/java/com/example/exerciciosdolucas/Exercicio4.kt
@@ -2,10 +2,32 @@ package com.example.exerciciosdolucas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.exerciciosdolucas.databinding.ActivityExercicio4Binding
+import java.text.NumberFormat
+import java.util.Locale
 
 class Exercicio4 : AppCompatActivity() {
+    private lateinit var binding: ActivityExercicio4Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_exercicio4)
+        binding = ActivityExercicio4Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonCalcular.setOnClickListener {
+            converter()
+        }
+    }
+
+    private fun converter() {
+
+        val editDolar = binding.editDolar
+        val formatDolar: NumberFormat = NumberFormat.getInstance(Locale.FRANCE)
+        val numberDolar: Number = formatDolar.parse(editDolar.text.toString())
+        val dDolar = numberDolar.toDouble()
+        val formula = dDolar * 4.87
+        val reais = binding.textReais
+        reais.text = String.format("%.3f", formula)
+
     }
 }
