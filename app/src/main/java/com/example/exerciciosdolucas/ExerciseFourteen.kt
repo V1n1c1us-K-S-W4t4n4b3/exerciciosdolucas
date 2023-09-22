d7d4df61b4194ccd5c560c7c05f2f7b15ae9aad9
V1n1c1us-KS-W4t4n4b3 <vinikswatanabe@outlook.com>
2023-09-21 21:27:57 -0300

    refazendo parte do programa

---
+++ app/src/main/java/com/example/exerciciosdolucas/ExerciseFourteen.kt
@@ -0,0 +1,11 @@
+package com.example.exerciciosdolucas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExerciseFourteen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercise_fourteen)
+    }
+}
